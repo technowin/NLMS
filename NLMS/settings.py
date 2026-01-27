59f28a9cebfc3fa956b9eda4d5a154534717d8a0
Pruthvi Narendra Hajare <pruthvi@technowin.co.in>
2026-01-27 13:42:34 +0530

    asdasasdsa

--- NLMS/settings.py
+++ NLMS/settings.py
@@ -95,8 +95,7 @@ DATABASES = {
         'NAME': 'nlms_db',      # Replace with your database name
         'USER': 'root',      # Replace with your database user
         'PASSWORD': 'Mysql_MH-047319',  # Replace with your database password
-        # 'HOST': '127.0.0.1',       # IP FOR LOCAL VM
-        'HOST': '3.111.76.175',
+        'HOST': '127.0.0.1',       # IP FOR LOCAL VM
         'PORT': '3306',            
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
@@ -107,8 +106,7 @@ DATABASES = {
         'NAME': 'L01_db',
         'USER': 'root',
         'PASSWORD': 'Mysql_MH-047319',
-        'HOST': '3.111.76.175',
-        # 'HOST': '127.0.0.1',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     },
     'L02': {
@@ -116,8 +114,7 @@ DATABASES = {
         'NAME': 'L02_db',
         'USER': 'root',
         'PASSWORD': 'Mysql_MH-047319',
-        'HOST': '3.111.76.175',
-        # 'HOST': '127.0.0.1',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     },
 }
@@ -286,24 +283,24 @@ STATICFILES_DIRS = [
    
 ]
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'file': {
-#             'level': 'WARNING',
-#             'class': 'logging.FileHandler',
-#             'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),  # Changed to relative path
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file'],
-#             'level': 'WARNING',
-#             'propagate': True,
-#         },
-#     },
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),  # Changed to relative path
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    },
+}
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     # Add any additional authentication backends if needed
